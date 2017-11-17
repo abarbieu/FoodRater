ca1054de280cdc57d54a2207134870bd5aab5cef
Aidan <happycamper108@gmail.com>
2017-11-17 08:57:28 -0800

    init phase3

---
+++ FoodRater/CellTV.swift
@@ -0,0 +1,26 @@
+//
+//  CellTV.swift
+//  FoodRater
+//
+//  Created by Mobile on 11/17/17.
+//  Copyright © 2017 Mobile. All rights reserved.
+//
+
+import UIKit
+
+class CellTV: UITableViewCell {
+
+    @IBOutlet weak var cellTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
